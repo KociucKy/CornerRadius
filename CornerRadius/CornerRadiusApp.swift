6c6fa86e63b292f5ed10b46f3822bc5ae89fa01d
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-08-01 13:52:15 +0200

    Initial Commit

---
+++ CornerRadius/CornerRadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerRadiusApp.swift
+//  CornerRadius
+//
+//  Created by Kuba Kociucki on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CornerRadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
