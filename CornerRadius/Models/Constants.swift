f1280a4f70eba664063ed6731b96a53eed5504b0
Jakub Kociucki <jakubkociucki.dev@gmail.com>
2021-08-01 13:57:38 +0200

    Implemented MVC and added all the needed files

---
+++ CornerRadius/Models/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CornerRadius
+//
+//  Created by Kuba Kociucki on 01/08/2021.
+//
+
+import Foundation
+
+enum General{
+    
+}
---
+++ CornerRadius/Views/InputFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  InputFieldView.swift
+//  CornerRadius
+//
+//  Created by Kuba Kociucki on 01/08/2021.
+//
+
+import SwiftUI
+
+struct InputFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputFieldView()
+    }
+}
